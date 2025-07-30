35b60b8567228c1f47dba66bb2983df44b17d172
abhisheknjfrog <abhishekn@jfrog.com>
2025-07-30 20:40:29 +0530

    Initial commit: Hello World package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/abhisheknjfrog/my-repo
+
+go 1.24.5
---
+++ hello.go
@@ -0,0 +1,9 @@
+package my-repo
+
+import "fmt"
+
+// Hello prints Hello World
+func Hello() {
+    fmt.Println("Hello, World!")
+}
+
